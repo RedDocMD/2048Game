db08f4db2e4728d98c6da8b998ddbeb1bf0a92ae
Deep Majumder <deep.majumder2019@gmail.com>
2020-07-08 14:00:39 +0530

    Now you can move, but only left

--- src/Board.kt
+++ src/Board.kt
@@ -2,6 +2,7 @@ import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Graphics2D
+import javax.swing.JOptionPane
 import javax.swing.JPanel
 import kotlin.random.Random
 
@@ -20,6 +21,14 @@ val colors = mapOf(
         2048 to Color(17, 74, 245)
 )
 
+enum class Direction {
+    LEFT, RIGHT, UP, DOWN
+}
+
+enum class GameState {
+    RUNNING, WON, LOST
+}
+
 data class Board(val rows: Int, val columns: Int) : JPanel() {
     private val tileWidth = 75.0
     private val tileHeight = 75.0
@@ -29,6 +38,16 @@ data class Board(val rows: Int, val columns: Int) : JPanel() {
     private val board: Array<Array<Int>>
     private val totalWidth = columns * tileWidth + (columns + 1) * horizontalSpace
     private val totalHeight = rows * tileHeight + (rows + 1) * verticalSpace
+
+    val gameState: GameState
+        get() {
+            val flattenedBoard = board.flatten()
+            return when {
+                2048 in flattenedBoard -> GameState.WON
+                0 !in flattenedBoard -> GameState.LOST
+                else -> GameState.RUNNING
+            }
+        }
 
     init {
         tiles = Array(rows) { Array(columns) { Tile(tileWidth, tileHeight, colors[0], "") } }
@@ -45,7 +64,7 @@ data class Board(val rows: Int, val columns: Int) : JPanel() {
         this[pos2 / rows, pos2 % rows] = 2
     }
 
-    operator fun set(i: Int, j: Int, number: Int) {
+    private operator fun set(i: Int, j: Int, number: Int) {
         board[i][j] = number
         tiles[i][j].tileColor = colors[number]
         tiles[i][j].tileContent = if (number == 0) "" else number.toString()
@@ -54,6 +73,86 @@ data class Board(val rows: Int, val columns: Int) : JPanel() {
 
     operator fun get(i: Int, j: Int): Int {
         return board[i][j]
+    }
+
+    fun move(direction: Direction) {
+        when (gameState) {
+            GameState.LOST -> JOptionPane.showMessageDialog(this,
+                    "Game has been lost!",
+                    "Game over",
+                    JOptionPane.INFORMATION_MESSAGE)
+            GameState.WON -> JOptionPane.showMessageDialog(this,
+                    "Game has been won!",
+                    "Game over",
+                    JOptionPane.INFORMATION_MESSAGE)
+            else -> {
+                // Deep copy of board
+                val newBoard = Array(rows) { i -> Array(columns) { j -> board[i][j] } }
+
+                when (direction) {
+                    Direction.LEFT -> {
+                        // First move the tiles
+                        for (i in 0 until rows) {
+                            var left = 0
+                            while (left < columns && newBoard[i][left] != 0) left++
+                            for (j in left until columns) {
+                                for (jj in j + 1 until columns) {
+                                    if (newBoard[i][jj] != 0) {
+                                        newBoard[i][j] = newBoard[i][jj]
+                                        newBoard[i][jj] = 0
+                                        break
+                                    }
+                                }
+                            }
+                        }
+                        // Then combine
+                        for (i in 0 until rows) {
+                            for (j in 0 until columns - 1) {
+                                if (newBoard[i][j] != 0 && newBoard[i][j] == newBoard[i][j + 1]) {
+                                    newBoard[i][j] *= 2
+                                    newBoard[i][j + 1] = 0
+                                    for (jj in j + 2 until columns) {
+                                        newBoard[i][jj - 1] = newBoard[i][jj]
+                                        newBoard[i][jj] = 0
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Choose a random square and randomly set it to 2 or 4
+                val random = Random.Default
+                val lim = rows * columns
+                var pos = random.nextInt(lim)
+                while (newBoard[pos / rows][pos % rows] != 0) pos = random.nextInt(lim)
+                val num = if (random.nextInt(2) == 0) 2 else 4
+                newBoard[pos / rows][pos % columns] = num
+
+                // Now update the actual board
+                for (i in 0 until rows) {
+                    for (j in 0 until columns) {
+                        val number = newBoard[i][j]
+                        board[i][j] = number
+                        tiles[i][j].tileColor = colors[number]
+                        tiles[i][j].tileContent = if (number == 0) "" else number.toString()
+                    }
+                }
+                repaint()
+
+                when (gameState) {
+                    GameState.LOST -> JOptionPane.showMessageDialog(this,
+                            "Game has been lost!",
+                            "No more moves",
+                            JOptionPane.INFORMATION_MESSAGE)
+                    GameState.WON -> JOptionPane.showMessageDialog(this,
+                            "Game has been won!",
+                            "No more moves",
+                            JOptionPane.INFORMATION_MESSAGE)
+                    else -> {}
+                }
+            }
+        }
     }
 
     override fun paintComponent(g: Graphics?) {
--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,6 @@
 import java.awt.EventQueue
+import java.awt.event.KeyAdapter
+import java.awt.event.KeyEvent
 import javax.swing.JFrame
 
 fun main() {
@@ -8,5 +10,18 @@ fun main() {
     frame.pack()
     frame.title = "2048 Game"
     frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    // Add the keyboard listener
+    frame.addKeyListener(object : KeyAdapter() {
+        override fun keyPressed(e: KeyEvent?) {
+            if (e != null) {
+                when (e.keyCode) {
+                    KeyEvent.VK_LEFT -> board.move(Direction.LEFT)
+                    KeyEvent.VK_RIGHT -> board.move(Direction.RIGHT)
+                    KeyEvent.VK_UP -> board.move(Direction.UP)
+                    KeyEvent.VK_DOWN -> board.move(Direction.DOWN)
+                }
+            }
+        }
+    })
     EventQueue.invokeLater { frame.isVisible = true }
 }
